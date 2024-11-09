85f60f9f10df68625ac9054370d58baec4a26265
NeoMetalx1 <153954385+NeoMetalx1@users.noreply.github.com>
2024-11-09 21:57:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import os
+from time import gmtime, strftime
 
 username = input("Enter user name: ")
+current_time = strftime("%a, %d %b %Y %H:%M:%S", gmtime())
+
 
 try:
     while True:
+        os.system("cat chat.txt")
         user_input = input("Enter text: ")
         if user_input.lower() == "exit":
             break
         
-        with open("chat.txt", "a", encoding="utf-8") as file:
-            file.write(f"[{username}] => {user_input}\n\n")
+        with open("chat.txt", "a", encoding="utf-8", errors="replace") as file:
+            file.write(f"[{username}] => {user_input}\n {current_time} \n\n")
         
         os.system("cat chat.txt")
 
